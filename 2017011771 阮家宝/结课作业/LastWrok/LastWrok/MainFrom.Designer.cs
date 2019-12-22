fd68feeb75109d1f0f96669163a012bf4ea3ce43
2017011771 阮家宝 <48323095+2017011771@users.noreply.github.com>
2019-12-22 14:55:51 +0800

    Add files via upload

---
+++ 2017011771 阮家宝/结课作业/LastWrok/LastWrok/MainFrom.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace LastWrok
+{
+    partial class MainFrom
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btOpen = new System.Windows.Forms.Button();
+            this.txDir = new System.Windows.Forms.TextBox();
+            this.lsbFiles = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btOpen
+            // 
+            this.btOpen.Location = new System.Drawing.Point(411, 80);
+            this.btOpen.Name = "btOpen";
+            this.btOpen.Size = new System.Drawing.Size(75, 39);
+            this.btOpen.TabIndex = 0;
+            this.btOpen.Text = "Open";
+            this.btOpen.UseVisualStyleBackColor = true;
+            this.btOpen.Click += new System.EventHandler(this.btOpen_Click);
+            // 
+            // txDir
+            // 
+            this.txDir.Location = new System.Drawing.Point(12, 12);
+            this.txDir.Multiline = true;
+            this.txDir.Name = "txDir";
+            this.txDir.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txDir.Size = new System.Drawing.Size(359, 107);
+            this.txDir.TabIndex = 1;
+            this.txDir.WordWrap = false;
+            this.txDir.TextChanged += new System.EventHandler(this.txDir_TextChanged);
+            // 
+            // lsbFiles
+            // 
+            this.lsbFiles.FormattingEnabled = true;
+            this.lsbFiles.ItemHeight = 15;
+            this.lsbFiles.Location = new System.Drawing.Point(12, 172);
+            this.lsbFiles.Name = "lsbFiles";
+            this.lsbFiles.Size = new System.Drawing.Size(392, 154);
+            this.lsbFiles.TabIndex = 2;
+            this.lsbFiles.SelectedIndexChanged += new System.EventHandler(this.lsbFiles_SelectedIndexChanged);
+            // 
+            // MainFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(540, 338);
+            this.Controls.Add(this.lsbFiles);
+            this.Controls.Add(this.txDir);
+            this.Controls.Add(this.btOpen);
+            this.Name = "MainFrom";
+            this.Text = "MainFrom";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btOpen;
+        private System.Windows.Forms.TextBox txDir;
+        private System.Windows.Forms.ListBox lsbFiles;
+    }
+}
+
---
+++ 2017011771 阮家宝/结课作业/LastWrok/LastWrok/MainFrom.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LastWrok
+{
+    public partial class MainFrom : Form
+    {
+        public MainFrom()
+        {
+            InitializeComponent();
+            searchedFiles = new List<string>();
+            searchComplete += new refreshListBoxHanlder(updateListBox);
+        }
+
+        private void btOpen_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fd = new FolderBrowserDialog();
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                txDir.Text = fd.SelectedPath;
+            }
+            Thread thread = new Thread(new ParameterizedThreadStart(searchFilesinFolder));
+            thread.Start(this);
+        }
+        public static void searchFilesinFolder(object obj)
+        {
+            string strDir = obj.ToString();
+            MainFrom thisForm = (MainFrom)obj;
+            strDir = thisForm.txDir.Text;
+            if (strDir != null && strDir.Length > 0)
+            {
+                if (Directory.Exists(strDir))
+                {
+                    searchedFiles.Clear();
+                    List<string> allSearcheFiles = new List<string>();
+                    List<string> folders2Search = new List<string>();
+                    folders2Search.Add(strDir);
+                    while (folders2Search.Count > 0)
+                    {
+                        try
+                        {
+                            string curFolder = folders2Search[0];
+                            folders2Search.RemoveAt(0);
+                            string[] files = Directory.GetFiles(curFolder);
+                            string[] dirs = Directory.GetDirectories(curFolder);
+                            foreach (string item in files)
+                                allSearcheFiles.Add(item);
+                            foreach (string item in dirs)
+                                folders2Search.Add(item);
+                        }
+                        catch { }
+                    }
+                    searchedFiles.AddRange(allSearcheFiles);
+                    refreshListBoxHanlder updateDelegate = new refreshListBoxHanlder(thisForm.updateListBox);
+                    thisForm.Invoke(updateDelegate, thisForm);
+                    thisForm.searchComplete(thisForm);
+                }
+            }
+        }
+        private void updateListBox(object sender)
+        {
+            if (this.InvokeRequired)
+            {
+                refreshListBoxHanlder delegateFunc = new refreshListBoxHanlder(updateListBox);
+                this.Invoke(delegateFunc, sender);
+            }
+            else
+            {
+                lsbFiles.Items.Clear();
+                lsbFiles.Items.AddRange(searchedFiles.ToArray());
+            }
+        }
+        public delegate void refreshListBoxHanlder(object sender);
+        public event refreshListBoxHanlder searchComplete;
+        public static List<string> searchedFiles;
+
+        private void txDir_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lsbFiles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
